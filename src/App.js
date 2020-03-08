f08ed918b4639f38a3e448561e50fa04a4e8c41f
dongolas <jodanous@gmail.com>
2020-03-08 21:27:57 +0200

    afterRedux

--- src/App.js
+++ src/App.js
@@ -1,81 +1,65 @@
-import React, { Component } from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import './App.css';
-import { ThemeProvider as MuiThemeProvider } from '@material-ui/core/styles';
-import createMuiTheme from '@material-ui/core/styles/createMuiTheme';
-//Components
-import Navbar from './components/Navbar';
-//Pages
-import home from './pages/home';
-import login from './pages/login';
-import signup from './pages/signup';
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import "./App.css";
+import { ThemeProvider as MuiThemeProvider } from "@material-ui/core/styles";
+import createMuiTheme from "@material-ui/core/styles/createMuiTheme";
+import themeFile from "./util/theme";
+import jwtDecode from "jwt-decode";
+// Redux
+import { Provider } from "react-redux";
+import store from "./redux/store";
+// Components
+import Navbar from "./components/Navbar";
+import AuthRoute from "./util/AuthRoute";
+// Pages
+import home from "./pages/home";
+import login from "./pages/login";
+import signup from "./pages/signup";
 
-const theme = createMuiTheme({
-  spacing: 1,
-  palette: {
-    primary: {
-      light: '#388e3c',
-      main: '#2e7d32',
-      dark: '#1b5e20',
-      contrastText: '#fff',
-    },
-    secondary: {
-      light: '#7b1fa2',
-      main: '#6a1b9a',
-      dark: '#4a148c',
-      contrastText: '#fff',
-    },
-  },
-  typography: {
-    useNextVariants: true
-  },
-  form: {
-    textAlign: 'center',
-  },
-  image: {
-      maxWidth: '100px',
-      margin: '20px auto',
-  },
-  pageTitle: {
-      margin: '10px auto'
-  },
-  textField: {
-      margin: '10px auto'
-  },
-  button: {
-      marginTop: '10px',
-      position: 'relative'
-  },
-  customError: {
-      color: 'red',
-      fontSize: '0.8rem',
-  },
-  progress: {
-      position: 'absolute'
+const theme = createMuiTheme(themeFile);
+
+let authenticated;
+const token = localStorage.FBIdToken;
+if (token) {
+  const decodedToken = jwtDecode(token);
+  console.log(decodedToken);
+  if (decodedToken.exp * 1000 < Date.now()) {
+    window.location.href = "/login";
+    authenticated = false;
+  } else {
+    authenticated = true;
   }
-})
+}
 
 class App extends Component {
   render() {
     return (
       <MuiThemeProvider theme={theme}>
-        <div className="App">
+        <Provider store={store}>
           <Router>
+            <Navbar />
             <div className="container">
-              <Navbar />
               <Switch>
-                <Route exact path="/" component={home}/>
-                <Route exact path="/login" component={login}/>
-                <Route exact path="/signup" component={signup}/>
+                <Route exact path="/" component={home} />
+                <AuthRoute
+                  exact
+                  path="/login"
+                  component={login}
+                  authenticated={authenticated}
+                />
+                <AuthRoute
+                  exact
+                  path="/signup"
+                  component={signup}
+                  authenticated={authenticated}
+                />
               </Switch>
             </div>
           </Router>
-        </div>
+        </Provider>
       </MuiThemeProvider>
     );
   }
 }
 
 export default App;
-
-//4:44:00
---
+++ src/util/theme.js
@@ -0,0 +1,19 @@
+export default {
+    palette: {
+      primary: {
+        light: '#388e3c',
+        main: '#2e7d32',
+        dark: '#1b5e20',
+        contrastText: '#fff',
+      },
+      secondary: {
+        light: '#7b1fa2',
+        main: '#6a1b9a',
+        dark: '#4a148c',
+        contrastText: '#fff',
+      },
+    },
+    typography: {
+      useNextVariants: true
+    },
+  }
